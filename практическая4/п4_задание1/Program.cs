0f11b4187a337905566e2cddb21d70d2615bbe79
falfeya <130572121+falfeya@users.noreply.github.com>
2023-04-17 12:49:52 +0500

    Add files via upload

---
+++ практическая4/п4_задание1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практическая4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string input = Console.ReadLine();
+            string[] words = SplitText(input);
+            DisplayWords(words);
+            Console.ReadKey();
+        }
+        static string[] SplitText(string text)
+        {
+            string[] words = text.Split(' ');
+            return words;
+        }
+        static void DisplayWords(string[] words)
+        {
+            Console.WriteLine("Слова в предложении:");
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);                
+            }
+        }
+    }
+}
+    
+
---
+++ практическая4/п4_задание2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace п4_задание2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputPhrase = Console.ReadLine();
+            string reversedPhrase = ReverseWords(inputPhrase);
+            Console.WriteLine("Разделение и обратная последовательность слов:");
+            Console.WriteLine(reversedPhrase);
+            Console.ReadKey();
+        }
+        static string ReverseWords(string inputPhrase)
+        {
+            string[] words = SplitWords(inputPhrase);
+
+            Array.Reverse(words);
+
+            return string.Join(" ", words);
+        }
+        static string[] SplitWords(string inputPhrase)
+        {
+            return inputPhrase.Split(' ');
+        }
+    }
+}
+    
+
